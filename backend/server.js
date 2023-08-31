53291881c986f387a34a308e4b9601e4f3653407
XSeann <seannejmalimpenet@gmail.com>
2023-08-30 21:21:08 -0700

    newAgain

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
-const {WebSocketServer} = require('ws')
+const WebSocket = require('ws')
 const cors = require('cors')
 
 const app = express()
