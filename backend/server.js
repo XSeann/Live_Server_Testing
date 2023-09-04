c42d3bfbe444ccc2bb3d3fb4a6f99fa6b86e8e10
XSeann <seannejmalimpenet@gmail.com>
2023-09-03 21:13:25 -0700

    ap

--- backend/server.js
+++ backend/server.js
@@ -64,6 +64,6 @@ io.on("connection", (socket) => {
   // receive a message from the client
   socket.on("hello from client", (args) => {
     //console.log(args)
-    io.emit('hello from server', 'okkkk')
+    io.emit('hello from server', args)
   });
 });
