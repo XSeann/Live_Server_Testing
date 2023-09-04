430bdf0316dc3fb8445077089496a0f7feaacb9f
XSeann <seannejmalimpenet@gmail.com>
2023-09-03 21:11:38 -0700

    sdkas

--- backend/server.js
+++ backend/server.js
@@ -63,7 +63,7 @@ io.on("connection", (socket) => {
 
   // receive a message from the client
   socket.on("hello from client", (args) => {
-    console.log(args)
+    //console.log(args)
     io.emit('hello from server', 'okkkk')
   });
 });
